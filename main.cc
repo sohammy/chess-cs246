e7b8ef775fa1fc307d3c7dfc21ddf016d7275884
sdsant <sdsant@uwaterloo.ca>
2023-11-27 11:57:28 -0500

    Added main file

---
+++ main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    cout << endl;
+    cout << "Welcome to Chess!" << endl;
+    cout << "a CS246 Project" << endl;
+    cout << "________________" << endl << endl;
+    cout << "________________" << endl << endl;
+
+
+
+    cout << "Play Again?" << endl;
+}
