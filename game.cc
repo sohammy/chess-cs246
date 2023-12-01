831857ebf3fd116347d2a43264db98370dc0856f
hlvance <hlvance@uwaterloo.ca>
2023-11-30 23:02:52 -0500

    blah

--- game.cc
+++ game.cc
@@ -59,9 +59,9 @@ void Game::play(Board& myBoard) {
 
     while (whoWon() == -1) { // Switches Turns Back and Forth between Black + White
         if (!whoseTurn) {
-            white->makeMove(); // getMove() should take in user input, only does move once 'move' is given as input (as per instructions)
+            white->makeMove(myBoard); // getMove() should take in user input, only does move once 'move' is given as input (as per instructions)
         } if (whoseTurn) {
-            black->makeMove(); // getMove() should take in user input, only does move once 'move' is given as input (as per instructions)
+            black->makeMove(myBoard); // getMove() should take in user input, only does move once 'move' is given as input (as per instructions)
         }
         whoseTurn = !whoseTurn;
     }
