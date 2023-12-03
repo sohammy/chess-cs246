18527acf7f40baa3b16b90e7e5bc147c2aa71747
sdsant <sdsant@uwaterloo.ca>
2023-12-02 21:38:00 -0500

    minor changes

--- bishop.cc
+++ bishop.cc
@@ -3,7 +3,6 @@
 Bishop::Bishop (vector<vector<Square>>& board, char pieceType) : Piece(board, pieceType)  {}
 
 void Bishop::calculateMoves() {
-    cout << "in bishop" << endl;
 
     // clear the arrays so that only relevant moves are saved
     possibleMoves.clear();
