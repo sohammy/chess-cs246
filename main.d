f4dac974e1f12bcfcb5a1fb9cd441af4d93d9274
hlvance <hlvance@uwaterloo.ca>
2023-12-05 20:04:39 -0500

    FINAL PUSH PROBABLY

--- main.d
+++
@@ -1,4 +0,0 @@
-main.o: main.cc game.h board.h square.h observer.h piece.h move.h king.h \
- queen.h rook.h knight.h bishop.h pawn.h textdisplay.h player.h human.h \
- computer.h minimax.h stageone.h stagetwo.h stagethree.h stagefour.h \
- stagefive.h graphicsdisplay.h window.h
